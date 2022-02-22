802fc8cf7fb87d4b09a80ad8a468487e53cbfee7
oye johnson <johnsonoye34@gmail.com>
2022-02-22 11:23:46 +0100

    chore: finished writing unit test for controllers

--- src/post/post.controller.spec.ts
+++ src/post/post.controller.spec.ts
@@ -5,16 +5,102 @@ import { PostService } from './post.service';
 describe('PostController', () => {
   let controller: PostController;
 
+  const mockPostService = {
+    create: jest.fn((dto) => {
+      return {
+        ...dto,
+      };
+    }),
+    update: jest.fn().mockImplementation((id, dto) => ({ id, ...dto })),
+    remove: jest.fn().mockImplementation((id) => ({
+      id,
+      title: 'oye is handsome',
+      description: 'very good lookin',
+    })),
+    findOne: jest.fn().mockImplementation((id) => ({
+      id,
+      title: 'oye is handsome',
+      description: 'very good lookin',
+    })),
+    findAll: jest.fn(() => {
+      return [
+        {
+          title: 'oye',
+          description: 'lekan',
+        },
+      ];
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostController],
       providers: [PostService],
-    }).compile();
+    })
+      .overrideProvider(PostService)
+      .useValue(mockPostService)
+      .compile();
 
     controller = module.get<PostController>(PostController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('it should create a post', () => {
+    expect(
+      controller.create({
+        description: 'lekan is good looking',
+        title: 'loves your',
+      }),
+    ).toEqual({
+      title: 'loves your',
+      description: 'lekan is good looking',
+    });
+
+    expect(mockPostService.create).toHaveBeenCalled();
+  });
+
+  it('should update a post', () => {
+    const dto = { description: 'lekan is good looking', title: 'loves your' };
+
+    expect(controller.update('1', dto)).toEqual({
+      id: 1,
+      ...dto,
+    });
+
+    expect(mockPostService.update).toHaveBeenCalled();
+  });
+
+  it('it should fetch all posts', () => {
+    expect(controller.findAll()).toEqual([
+      {
+        title: 'oye',
+        description: 'lekan',
+      },
+    ]);
+
+    expect(mockPostService.findAll).toHaveBeenCalled();
+  });
+
+  it('it should fetch on post', () => {
+    expect(controller.findOne('1')).toEqual({
+      id: 1,
+      title: 'oye is handsome',
+      description: 'very good lookin',
+    });
+
+    expect(mockPostService.findOne).toHaveBeenCalled();
+  });
+
+  it('remove post', () => {
+    expect(controller.remove('1')).toEqual({
+      id: 1,
+      title: 'oye is handsome',
+      description: 'very good lookin',
+    });
+
+    expect(mockPostService.remove).toHaveBeenCalled();
   });
 });
--- src/post/post.service.spec.ts
+++ src/post/post.service.spec.ts
@@ -1,12 +1,33 @@
+import { Post } from './entities/post.entity';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { PostService } from './post.service';
 
 describe('PostService', () => {
   let service: PostService;
 
+  const mockPostRepository = {
+    create: jest.fn().mockImplementation((dto) => dto),
+    update: jest.fn().mockImplementation((dto) => dto),
+    remove: jest.fn().mockImplementation((dto) => dto),
+    findOne: jest.fn().mockImplementation((dto) => dto),
+    findAll: jest
+      .fn()
+      .mockImplementation(() => [
+        { title: 'oye', description: 'oyelamilekan' },
+      ]),
+    save: jest.fn().mockImplementation((post) => Promise.resolve({ ...post })),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostService],
+      providers: [
+        PostService,
+        {
+          provide: getRepositoryToken(Post),
+          useValue: mockPostRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<PostService>(PostService);
@@ -15,4 +36,28 @@ describe('PostService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create a new user record and return that', async () => {
+    expect(
+      await service.create({
+        title: 'Oye is handsome',
+        description: 'very good looking fellow',
+      }),
+    ).toEqual({
+      title: 'Oye is handsome',
+      description: 'very good looking fellow',
+    });
+  });
+
+  it("should get all post",async () => {
+    expect(
+      await service.create({
+        title: 'Oye is handsome',
+        description: 'very good looking fellow',
+      }),
+    ).toEqual({
+      title: 'Oye is handsome',
+      description: 'very good looking fellow',
+    });
+  })
 });
